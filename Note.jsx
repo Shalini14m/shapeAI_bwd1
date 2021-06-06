99de45a467132a6022d953516ba362131871e419
Shalini Mudaliar <84931416+Shalini14m@users.noreply.github.com>
2021-06-07 01:04:38 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+
+function Note() {
+   return(
+    
+      <div className="note">
+           <h1>EC Engineering</h1>
+           <p>This stream of engngineering is an amazing stream.In this stream we can learn both programming and electronics.Well i like programming more than electronics :))</p>
+      </div>
+    
+
+  );
+}
+
+export default Note;     
